817bd4aa2e5c7b60230d1bd943d0f165bcbcc45e
wonderpic <wonderpic@gmail.com>
2026-05-29 01:15:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-console.log("Coffee Farm AI RUNNING");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAo0zYNRGCa-XaNZgqTijz0P6uz_76pKGQ",
+  authDomain: "coffee-farm-ai.firebaseapp.com",
+  projectId: "coffee-farm-ai",
+  storageBucket: "coffee-farm-ai.firebasestorage.app",
+  messagingSenderId: "450326520755",
+  appId: "1:450326520755:web:19e3ed232f919adf6aa1c6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firebase connected ✔");
 
 document.getElementById("status").innerText =
-  "App berhasil jalan ✔";
+  "App + Firebase ready ✔";

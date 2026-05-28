12b50eca0f41614e064441f74d64b03150d35470
wonderpic <wonderpic@gmail.com>
2026-05-29 01:29:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-console.log("SCRIPT JALAN");
+console.log("SCRIPT KELOAD");
 
 document.body.innerHTML = `
   <h1>☕ Coffee Farm AI</h1>
-  <p id="status">Loading Firebase...</p>
+  <p id="status">Loading...</p>
 `;
 
 document.getElementById("status").innerText = "App hidup ✔";

42b9a2daedfcc55358de3882b57a5a21cb8481ab
wonderpic <wonderpic@gmail.com>
2026-05-29 00:47:48 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAo0zYNRGCa-XaNZgqTijz0P6uz_76pKGQ",
+  authDomain: "coffee-farm-ai.firebaseapp.com",
+  projectId: "coffee-farm-ai",
+  storageBucket: "coffee-farm-ai.firebasestorage.app",
+  messagingSenderId: "450326520755",
+  appId: "1:450326520755:web:19e3ed232f919adf6aa1c6",
+  measurementId: "G-8YPEE20H46"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
